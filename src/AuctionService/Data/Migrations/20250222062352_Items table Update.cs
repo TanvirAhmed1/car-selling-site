97bccd8a453571605e5f33fb9fd5b1708074d1bf
TanvirAhmed1 <tanvirnoor38@yahoo.com>
2025-02-22 12:40:04 +0600

    Db and Seed Data Added

---
+++ src/AuctionService/Data/Migrations/20250222062352_Items table Update.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ItemstableUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Milage",
+                table: "Items",
+                newName: "Mileage");
+
+                migrationBuilder.Sql("ALTER TABLE \"Items\" ALTER COLUMN \"Year\" TYPE integer USING \"Year\"::integer;");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Year",
+                table: "Items",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Mileage",
+                table: "Items",
+                newName: "Milage");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Year",
+                table: "Items",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+    }
+}
