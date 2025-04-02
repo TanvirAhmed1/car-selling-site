606755bf6355956f6b866396dcd6166424db4aa2
TanvirAhmed1 <tanvirnoor38@yahoo.com>
2025-04-03 01:24:34 +0600

    Dto and AutoMapper Added

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04428707b83f08312985176c1078bf82bf654d73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97bccd8a453571605e5f33fb9fd5b1708074d1bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
